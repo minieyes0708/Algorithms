fc649ee479bba26225624d7b871efbcdaa2291f5
minieyes <chenvey2@gmail.com>
2021-10-31 18:08:43 +0800

    done hello project

--- hello/RandomWord.java
+++ hello/RandomWord.java
@@ -5,12 +5,23 @@ import edu.princeton.cs.algs4.StdRandom;
 public class RandomWord {
     public static void main(String[] args) {
         if (StdIn.isEmpty()) {
-            System.out.println("heads tails");
+            StdOut.println("heads tails");
             if (StdRandom.bernoulli(0.5))
-                System.out.println("heads");
+                StdOut.println("heads");
             else
-                System.out.println("tails");
+                StdOut.println("tails");
             return;
+        } else {
+            int count = 1;
+            String result = StdIn.readString();
+            while (!StdIn.isEmpty()) {
+                count++;
+                String next = StdIn.readString();
+                if (StdRandom.bernoulli(1.0 / count)) {
+                    result = next;
+                }
+            }
+            StdOut.println(result);
         }
     }
 }
--- init.ps1
+++ init.ps1
@@ -1,15 +1,29 @@
-$basepath = "C:\Users\chenv\OneDrive\文件\programs\java\AlgorithmsPart1"
-function make {
-    $options = @(
-        'build and run'
-    )
-    $option = $($options -join '`n' | fzf)
+$workspace = "C:\Users\chenv\OneDrive\文件\programs\java\AlgorithmsPart1"
 
-    switch ($option) {
-        'build and run' {
-            $target = $($args[0]);
-            javac -cp "~\workspace\algs4.jar" "$target.java";
-            java -cp "~\workspace\algs4.jar;." $target;
-        }
+function build {
+    param (
+        $target = $nil
+    )
+    if (!$target) {
+        $targets = Get-ChildItem *.java | %{[io.path]::GetFileNameWithoutExtension($_.Name)};
+        $target = $($targets | fzf);
+    }
+    if (!$target) {return;}
+
+    javac -cp "$workspace\algs4.jar" "$target.java";
+}
+function run {
+    param ( $target = $nil, $infile = $nil)
+
+    if (!$target) {
+        $targets = Get-ChildItem *.java | %{[io.path]::GetFileNameWithoutExtension($_.Name)};
+        $target = $($targets | fzf);
+    }
+    if (!$target) {return;}
+
+    if ($infile) {
+        Get-Content $infile | java -cp "$workspace\algs4.jar;." $target;
+    } else {
+        java -cp "$workspace\algs4.jar;." $target;
     }
 }
