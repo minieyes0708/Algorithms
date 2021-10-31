68a5c1a1e76739624e1ef175f3bea5e20b5570b6
minieyes <chenvey2@gmail.com>
2021-10-31 18:22:20 +0800

    head, tail was manual input...

--- hello/RandomWord.java
+++ hello/RandomWord.java
@@ -4,24 +4,15 @@ import edu.princeton.cs.algs4.StdRandom;
 
 public class RandomWord {
     public static void main(String[] args) {
-        if (StdIn.isEmpty()) {
-            StdOut.println("heads tails");
-            if (StdRandom.bernoulli(0.5))
-                StdOut.println("heads");
-            else
-                StdOut.println("tails");
-            return;
-        } else {
-            int count = 1;
-            String result = StdIn.readString();
-            while (!StdIn.isEmpty()) {
-                count++;
-                String next = StdIn.readString();
-                if (StdRandom.bernoulli(1.0 / count)) {
-                    result = next;
-                }
+        int count = 0;
+        String result = "";
+        while (!StdIn.isEmpty()) {
+            count++;
+            String next = StdIn.readString();
+            if (StdRandom.bernoulli(1.0 / count)) {
+                result = next;
             }
-            StdOut.println(result);
         }
+        StdOut.println(result);
     }
 }
