1e722a488b49a61354447aefc9d5e7692c550262
minieyes <chenvey2@gmail.com>
2021-11-06 20:44:09 +0800

    use ufFull

--- percolation/Percolation.java
+++ percolation/Percolation.java
@@ -10,6 +10,7 @@ public class Percolation {
     private final int ufSize;
     private boolean[][] isOpenFlags;
     private final WeightedQuickUnionUF uf;
+    private final WeightedQuickUnionUF ufFull;
     // creates n-by-n grid, with all sites initially blocked
     public Percolation(int n)
     {
@@ -30,6 +31,7 @@ public class Percolation {
         // with additional top & bottom virtual site
         this.ufSize = n * n + 2;
         this.uf = new WeightedQuickUnionUF(this.ufSize);
+        this.ufFull = new WeightedQuickUnionUF(this.ufSize);
     }
     private void connect(int row1, int col1, int row2, int col2)
     {
@@ -39,6 +41,7 @@ public class Percolation {
         if (ind2 < 0 || ind2 >= ufSize) throw new IllegalArgumentException();
 
         uf.union(ind1, ind2);
+        if (ind1 != ufSize && ind2 != ufSize) ufFull.union(ind1, ind2);
     }
     // opens the site (row, col) if it is not open already
     public void open(int row, int col)
@@ -81,13 +84,22 @@ public class Percolation {
 
         return uf.find(ind1) == uf.find(ind2);
     }
+    private boolean isConnectedFull(int row1, int col1, int row2, int col2)
+    {
+        int ind1 = (row1 - 1) * n + (col1 - 1) + 1;
+        int ind2 = (row2 - 1) * n + (col2 - 1) + 1;
+        if (ind1 < 0 || ind1 >= ufSize) throw new IllegalArgumentException();
+        if (ind2 < 0 || ind2 >= ufSize) throw new IllegalArgumentException();
+
+        return ufFull.find(ind1) == ufFull.find(ind2);
+    }
     // is the site (row, col) full?
     public boolean isFull(int row, int col)
     {
         if (row < 1 || row > n) throw new IllegalArgumentException();
         if (col < 1 || col > n) throw new IllegalArgumentException();
 
-        return isConnected(row, col, topRow, topCol);
+        return isConnectedFull(row, col, topRow, topCol);
     }
     // returns the number of open sites
     public int numberOfOpenSites()
