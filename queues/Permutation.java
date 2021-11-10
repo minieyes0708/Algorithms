75b1460333cf8c9878a87f30ec87f1a5dc2cb5ea
minieyes <chenvey2@gmail.com>
2021-11-10 21:59:52 +0800

    only need max k count of objects in Permutation.java

--- queues/Permutation.java
+++ queues/Permutation.java
@@ -1,21 +1,28 @@
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
 
 public class Permutation {
     public static void main(String[] args) {
         int maxCount = Integer.parseInt(args[0]);
         RandomizedQueue<String> queue = new RandomizedQueue<String>();
 
-        while (!StdIn.isEmpty()) {
-            queue.enqueue(StdIn.readString());
-        }
-        int count = 0;
         if (maxCount == 0) return;
+
+        int count = 0;
+        while (!StdIn.isEmpty()) {
+            count++;
+            if (count <= maxCount) {
+                queue.enqueue(StdIn.readString());
+            } else {
+                if (StdRandom.bernoulli((double) maxCount / count)) {
+                    queue.dequeue();
+                    queue.enqueue(StdIn.readString());
+                }
+            }
+        }
         for (String str : queue) {
             StdOut.println(str);
-            count++;
-            if (count == maxCount)
-                break;
         }
     }
 }
