218c1f975d9fe237dc529e8449bfe4e5aa3cef4f
minieyes <chenvey2@gmail.com>
2021-11-16 20:32:27 +0800

    use PointWithSlopeBag to record added slopes

--- collinear/FastCollinearPoints.java
+++ collinear/FastCollinearPoints.java
@@ -1,21 +1,36 @@
 import java.util.Arrays;
 import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.Bag;
 import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdDraw;
 
 public class FastCollinearPoints {
-    private int startind, endind;
+    private class PointWithSlopeBag {
+        public Point point;
+        public Bag<Double> slopes;
+        public PointWithSlopeBag(Point pt) {
+            point = pt;
+            slopes = new Bag<Double>();
+        }
+    }
     private int segmentCount = 0;
+    private int startind, endind, curind;
     private LineSegment[] lineSegments = null;
+    private PointWithSlopeBag[] pointsAndSlopes = null;
     public FastCollinearPoints(Point[] points) {
         if (points == null) throw new IllegalArgumentException();
         for (Point pt : points) if (pt == null) throw new IllegalArgumentException();
 
-        int curind = 0;
-        while (curind < points.length) {
-            Arrays.sort(points, curind + 1, points.length, points[curind].slopeOrder());
+        pointsAndSlopes = new PointWithSlopeBag[points.length];
+        for (int i = 0; i < points.length; i++)
+            pointsAndSlopes[i] = new PointWithSlopeBag(points[i]);
+
+        for (curind = 0; curind < points.length; curind++) {
+            Arrays.sort(points, 0, points.length, pointsAndSlopes[curind].point.slopeOrder());
+
+            endind = -1;
+            startind = -1;
             findLinePoints(points, curind);
-            curind = moveLinePoints(points, curind);
         }
     }
     public int numberOfSegments() {
@@ -28,13 +43,11 @@ public class FastCollinearPoints {
         return result;
     }
     private void findLinePoints(Point[] points, int curind) {
-        endind = -1;
-        startind = -1;
-        for (int i = curind + 1; i < points.length - 1; i++)
+        for (int i = 1; i < points.length - 1; i++)
         {
-            if (points[curind].slopeTo(points[i]) == Double.NEGATIVE_INFINITY) throw new IllegalArgumentException();
-            if (points[curind].slopeTo(points[i + 1]) == Double.NEGATIVE_INFINITY) throw new IllegalArgumentException();
-            if (points[curind].slopeTo(points[i]) == points[curind].slopeTo(points[i + 1]))
+            if (points[0].slopeTo(points[i]) == Double.NEGATIVE_INFINITY) throw new IllegalArgumentException();
+            if (points[0].slopeTo(points[i + 1]) == Double.NEGATIVE_INFINITY) throw new IllegalArgumentException();
+            if (points[0].slopeTo(points[i]) == points[0].slopeTo(points[i + 1]))
             {
                 if (startind == -1)
                     startind = i;
@@ -42,36 +55,27 @@ public class FastCollinearPoints {
             }
             else
             {
-                if (startind != -1 && endind - startind > 2) {
-                    Point pt1 = points[curind], pt2 = points[curind];
-                    for (int j = startind; j < endind; j++) {
-                        if (points[j].compareTo(pt1) < 0) pt1 = points[j];
-                        if (points[j].compareTo(pt2) > 0) pt2 = points[j];
+                if (startind != -1) {
+                    if (endind - startind > 2) {
+                        Point pt1 = points[0], pt2 = points[0];
+                        for (int j = startind; j < endind; j++) {
+                            if (points[j].compareTo(pt1) < 0) pt1 = points[j];
+                            if (points[j].compareTo(pt2) > 0) pt2 = points[j];
+                        }
+                        addSegment(pt1, pt2);
                     }
-                    addSegment(pt1, pt2);
+                    endind = -1;
+                    startind = -1;
                 }
-                endind = -1;
-                startind = -1;
             }
         }
         if (startind != -1 && endind - startind > 2) {
-            Point pt1 = points[curind], pt2 = points[curind];
+            Point pt1 = points[0], pt2 = points[0];
             for (int i = startind; i < endind; i++) {
                 if (points[i].compareTo(pt1) < 0) pt1 = points[i];
                 if (points[i].compareTo(pt2) > 0) pt2 = points[i];
             }
             addSegment(pt1, pt2);
-        }
-    }
-    private int moveLinePoints(Point[] points, int curind) {
-        if (startind != -1) {
-            for (int i = 0; i < endind - startind; i++) {
-                swap(points, curind + i + 1, i + startind);
-            }
-            return endind;
-        }
-        else {
-            return curind + 1;
         }
     }
     private void swap(Point[] points, int ind1, int ind2) {
@@ -86,10 +90,22 @@ public class FastCollinearPoints {
         lineSegments = newLineSegments;
     }
     private void addSegment(Point pt1, Point pt2) {
-        if (lineSegments == null)
+        boolean findSlope = false;
+        for (double slope : pointsAndSlopes[curind].slopes) {
+            if (slope == pt1.slopeTo(pt2)) {
+                findSlope = true;
+                break;
+            }
+        }
+        if (findSlope) {
+            return;
+        }
+        if (lineSegments == null) {
             lineSegments = new LineSegment[1];
-        if (segmentCount == lineSegments.length - 1)
+        }
+        if (segmentCount == lineSegments.length - 1) {
             resize(lineSegments.length * 2);
+        }
         lineSegments[segmentCount++] = new LineSegment(pt1, pt2);
     }
     public static void main(String[] args) {
@@ -107,12 +123,14 @@ public class FastCollinearPoints {
         StdDraw.enableDoubleBuffering();
         StdDraw.setXscale(0, 32768);
         StdDraw.setYscale(0, 32768);
+        StdDraw.setPenRadius(0.005);
         for (Point p : points) {
             p.draw();
         }
         StdDraw.show();
 
         // print and draw the line segments
+        StdDraw.setPenRadius(0.001);
         FastCollinearPoints collinear = new FastCollinearPoints(points);
         for (LineSegment segment : collinear.segments()) {
             StdOut.println(segment);
