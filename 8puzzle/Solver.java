ba6c40fb7f37eaa3fac02344635b4f072cd296ac
Minieyes Chen <chenvey2@gmail.com>
2021-11-25 19:57:59 +0800

    use twin to find unsolvable

--- 8puzzle/Solver.java
+++ 8puzzle/Solver.java
@@ -1,9 +1,8 @@
-import java.util.Iterator;
 import java.util.Comparator;
 import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.Queue;
 import edu.princeton.cs.algs4.MinPQ;
 import edu.princeton.cs.algs4.StdOut;
-import java.util.NoSuchElementException;
 
 public class Solver {
     private class BoardInfo {
@@ -18,26 +17,6 @@ public class Solver {
             manhattan = -1;
         }
     }
-    private class SolutionIterable implements Iterable<Board> {
-        private class SolutionIterator implements Iterator<Board> {
-            BoardInfo info = brdSolution;
-            public boolean hasNext() {
-                return info != null;
-            }
-            public Board next() {
-                if (!hasNext()) throw new NoSuchElementException();
-                Board brd = info.board;
-                info = info.parent;
-                return brd;
-            }
-            public void remove() {
-                throw new UnsupportedOperationException();
-            }
-        }
-        public Iterator<Board> iterator() {
-            return new SolutionIterator();
-        }
-    }
     private class ManhattanComparator implements Comparator<BoardInfo> {
         public int compare(BoardInfo info1, BoardInfo info2) {
             if (info1.manhattan == -1) info1.manhattan = info1.board.manhattan();
@@ -47,65 +26,72 @@ public class Solver {
     }
     private final int numOfMoves;
     private final BoardInfo brdSolution;
-    private final SolutionIterable iterSolution = new SolutionIterable();
-    // find a solution to the initial board (using the A* algorithm)
     public Solver(Board initial) {
         if (initial == null) throw new IllegalArgumentException();
 
         MinPQ<BoardInfo> pq = new MinPQ<BoardInfo>(new ManhattanComparator());
+        MinPQ<BoardInfo> twinpq = new MinPQ<BoardInfo>(new ManhattanComparator());
         pq.insert(new BoardInfo(0, initial, null));
+        twinpq.insert(new BoardInfo(0, initial.twin(), null));
         while (!pq.isEmpty()) {
-            // StdOut.println("==========");
-            // for (BoardInfo ifo : pq) {
-                // StdOut.printf("%s moves = %d, manhattan = %d\n",
-                    // ifo.board.toString(),
-                    // ifo.moves, ifo.manhattan);
-            // }
             BoardInfo info = pq.delMin();
-            if (info.board.isGoal())
-            {
+            BoardInfo twininfo = twinpq.delMin();
+            if (twininfo.board.isGoal()) {
+                break;
+            }
+            if (info.board.isGoal()) {
+                brdSolution = buildSolution(info);
                 numOfMoves = info.moves;
-
-                BoardInfo curr = info;
-                BoardInfo next = null;
-                BoardInfo prev = null;
-                while (curr != null) {
-                    next = curr.parent;
-                    curr.parent = prev;
-                    prev = curr;
-                    curr = next;
-                }
-                brdSolution = prev;
                 return;
             }
-            for (Board neighbor : info.board.neighbors()) {
-                boolean found = false;
-                BoardInfo curinfo = info;
-                while (true) {
-                    if (curinfo.parent == null) break;
-                    if (neighbor.equals(curinfo.parent.board)) {
-                        found = true;
-                        break;
-                    }
-                    curinfo = curinfo.parent;
-                }
-                if (!found)
-                    pq.insert(new BoardInfo(info.moves + 1, neighbor, info));
-            }
+            addNeighbors(info, pq);
+            addNeighbors(twininfo, twinpq);
         }
         numOfMoves = -1;
         brdSolution = null;
     }
+    private void addNeighbors(BoardInfo info, MinPQ<BoardInfo> pq) {
+        for (Board neighbor : info.board.neighbors()) {
+            boolean found = false;
+            BoardInfo curinfo = info;
+            while (true) {
+                if (curinfo.parent == null) break;
+                if (neighbor.equals(curinfo.parent.board)) {
+                    found = true;
+                    break;
+                }
+                curinfo = curinfo.parent;
+            }
+            if (!found)
+                pq.insert(new BoardInfo(info.moves + 1, neighbor, info));
+        }
+    }
+    private static BoardInfo buildSolution(BoardInfo info) {
+        BoardInfo curr = info;
+        BoardInfo next = null;
+        BoardInfo prev = null;
+        while (curr != null) {
+            next = curr.parent;
+            curr.parent = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
     public boolean isSolvable() {
         return brdSolution != null;
     }
-    // min number of moves to solve initial board; -1 if unsolvable
     public int moves() {
         return numOfMoves;
     }
-    // sequence of boards in a shortest solution; null if unsolvable
     public Iterable<Board> solution() {
-        return iterSolution;
+        Queue<Board> result = new Queue<Board>();
+        BoardInfo curbrd = brdSolution;
+        while (curbrd != null) {
+            result.enqueue(curbrd.board);
+            curbrd = curbrd.parent;
+        }
+        return result;
     }
     public static void main(String[] args) {
         // create initial board from file
